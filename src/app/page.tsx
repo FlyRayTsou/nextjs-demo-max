b8eb9feadd22fa0ff2c578d74082565a415c04ea
Ray <rayflyworld@gmail.com>
2025-02-17 11:59:34 +0900

    divide header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Link from 'next/link'
 
+import Header from "@/components/header";
+
 export default function Home() {
   console.log("...executing")
   return (
     <main>
+      <Header />
       <p><Link href="/about">About us</Link></p>
     </main>
   );
