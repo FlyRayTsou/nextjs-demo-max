07097874e43feddb30b878cc11f43f415ce1878d
Ray <rayflyworld@gmail.com>
2025-02-27 15:42:24 +0900

    active nav link

--- 02-onwards-foodies-starting-project/components/main-header/main-header.js
+++ 02-onwards-foodies-starting-project/components/main-header/main-header.js
@@ -1,26 +1,30 @@
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
 import LogoImg from '@/assets/logo.png';
+import { usePathname } from 'next/navigation';
 
 import classes from './main-header.module.css';
 import MainHeaderBackground from './main-header-background';
 
 export default function MainHeader({children}) {
+    const path = usePathname();
     return (
     <>
       <MainHeaderBackground />
       <header className={classes.header}>
-        <Link　className={classes.logo} href="/">
+        <Link className={classes.logo} href="/">
             <Image src={LogoImg} alt="Food Logo" priority />
             NextLevel Food
         </Link>
         <nav className={classes.nav}>
             <ul>
                 <li>
-                    <Link href="/meals">Meals</Link>
+                    <Link href="/meals" className={path.startsWith('/meals') ? classes.active : undefined}>Meals</Link>
                 </li>
                 <li>
-                    <Link href="/community">Community</Link>
+                    <Link href="/community" className={path.startsWith('/community') ? classes.active : undefined}>Community</Link>
                 </li>
             </ul>
         </nav>
